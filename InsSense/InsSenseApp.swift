1ab3b5bebc17c0529900816641b83de6f5072d60
rezaaskary <reza.askary99@gmail.com>
2026-03-29 02:28:32 -0500

    Initial Commit

---
+++ InsSense/InsSenseApp.swift
@@ -0,0 +1,32 @@
+//
+//  InsSenseApp.swift
+//  InsSense
+//
+//  Created by Controllab on 3/29/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InsSenseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
